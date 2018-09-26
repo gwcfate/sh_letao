a9a47187409cbd2aaf5e4e680b6bb0d7820b5d29
gwc <2453316835@qq.com>
2018-09-26 22:00:09 +0800

    表单ajax验证还差点完成

--- public/back1/js/login.js
+++ public/back1/js/login.js
@@ -17,8 +17,8 @@ $(function () {
                         message: "用户名不能为空"
                     },
                     stringLength: {
-                        min: 6,
-                        max: 30,
+                        min: 2,
+                        max: 6,
                         message: '用户名长度必须2-6位'
                     }
                 }
@@ -37,5 +37,44 @@ $(function () {
             }
         }
     });
+
+
+    $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    //使用ajax提交逻辑
+
+    $.ajax({
+        type: "post",
+        url: "/employee/employeeLogin",
+        data: $('#form').serialize(),
+        dataType: "json",
+        success: function (info) {
+            if (info.success) {
+                location.href = "index.html";
+            }
+            if (info.error === 1000) {
+                alert("用户名不存在");
+            }
+            if (info.error === 1001) {
+                alert("密码错误");
+            }
+        }
+    })
+
+    });
+
+    
+    $('[type="reset"]').click(function () {
+        $('#form').data('bootstrapValidator').resetForm();
+    })
+
+
+
+
+
+
+
+
+
 
 });
