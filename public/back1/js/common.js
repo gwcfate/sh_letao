145261da8e532256344ea663e2ac21bfe99aa0e9
gwc <2453316835@qq.com>
2018-09-28 17:33:39 +0800

    接下来分页

--- public/back1/js/common.js
+++ public/back1/js/common.js
@@ -6,3 +6,45 @@ $(document).ajaxStart(function () {
 $(document).ajaxStop(function () {
     NProgress.done();
 });
+
+
+//公共效果
+$(function () {
+    // 二级菜单切换
+    $('.lt_aside .category').click(function () {
+        $('.lt_aside .child').stop().slideToggle();
+    });
+
+    $('.icon_menu').click(function () {
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+    });
+
+    // js插件模态框
+    $('.icon_logout').click(function () {
+        $('#logoutModal').modal('show');
+    });
+    
+    $('#logoutBtn').click(function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function (info) {
+                // console.log(info);
+                if (info.success) {
+                    location.href = "login.html";
+                }
+                
+            }
+        })
+    })
+
+
+
+
+
+
+
+})
---
+++ public/back1/js/index.js
@@ -0,0 +1,95 @@
+$(function () {
+    var echarts1 = echarts.init(document.querySelector('.echarts_1'));
+
+    // 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: '2017年 注册人数',
+            textStyle: {
+                color: 'red'
+            }
+        },
+        tooltip: {},
+        legend: {
+            data: ['人数']
+        },
+        xAxis: {
+            data: ["1月", "2月", "3月", "4月", "5月", "6月"]
+        },
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [1000, 1500, 2500, 1800, 2300, 2000]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    echarts1.setOption(option1);
+
+
+
+    var echarts2 = echarts.init(document.querySelector('.echarts_2'));
+
+    // 指定图表的配置项和数据
+    var option2 = {
+        title: {
+            text: '热门品牌销售',
+            subtext: '2017年6月',
+            x: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['奶可', '阿迪', '李宁', '李宁王', '阿迪王']
+        },
+        series: [
+            {
+                name: '品牌销量',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    { value: 335, name: '奶可' },
+                    { value: 310, name: '阿迪' },
+                    { value: 234, name: '李宁' },
+                    { value: 135, name: '李宁王' },
+                    { value: 1548, name: '阿迪王' }
+                ],
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'skyblue'
+                    }
+                }
+            }
+        ]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    echarts2.setOption(option2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
